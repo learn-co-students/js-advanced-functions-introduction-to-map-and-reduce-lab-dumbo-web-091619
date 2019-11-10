df70440553a14635543dcabbf7d98a4c8badd2c2
Noam Sauer-Utley <noamsauerutley@gmail.com>
2019-11-10 09:43:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
 // Your code here
+// map-like methods
+const mapToNegativize = (arr) => {
+    let newArr = []
+    arr.forEach(element => {
+       newArr.push(-element)
+    })
+    return newArr
+}
+
+const mapToNoChange = (arr) => {
+    let newArr = []
+    arr.forEach(element => {
+        newArr.push(element)
+    })
+    return newArr
+}
+
+const mapToDouble = (arr) => {
+    let newArr = []
+    arr.forEach(element => {
+        newArr.push(element * 2)
+    })
+    return newArr
+}
+
+const mapToSquare = (arr) => {
+    let newArr = []
+    arr.forEach(element => {
+        newArr.push(element * element)
+    })
+    return newArr
+}
+
+// reduce methods
+const reduceToTotal = (arr, num = 0) => {
+    arr.forEach(element => {
+        num +=element
+    })
+    return num
+}
+
+const reduceToAllTrue = (arr) => {
+    let valueArray = []
+   arr.forEach(element =>{
+       if(element){
+           valueArray.push(true)
+       } else { valueArray.push(false)}
+   })
+   return true ? !valueArray.includes(false) : false
+}
+
+const reduceToAnyTrue = (arr) => {
+    let valueArray = []
+   arr.forEach(element =>{
+       if(element){
+           valueArray.push(true)
+       } else { valueArray.push(false)}
+   })
+return true ? valueArray.includes(true) : false
+}
